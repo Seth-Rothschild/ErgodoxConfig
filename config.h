aef86268d82fbad391f809cdebec878e7d61df04
Seth Rothschild <seth.j.rothschild@gmail.com>
2019-08-13 21:28:10 -0400

    Sticky modifiers

---
+++ config.h
@@ -0,0 +1,6 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
